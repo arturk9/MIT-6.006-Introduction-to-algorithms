7dbbdb87bf6b64e94cee6138f582aeed7ad374a6
arturk9 <artkul19@gmail.com>
2016-12-19 21:23:36 +0100

    Insertion Sort for array of integers

---
+++ InsertionSort/InsertionSort.java
@@ -0,0 +1,19 @@
+package InsertionSort;
+
+public final class InsertionSort {
+
+    private InsertionSort(){};
+
+    public static int[] sort(int[] list){
+        for(int j = 0; j < list.length; j++){
+            int key =  list[j];
+            int i = j - 1;
+            while(i > -1 && list[i] > key){
+                list[i+1] = list[i];
+                i = i-1;
+            }
+            list[i+1] = key;
+        }
+        return list;
+    }
+}
